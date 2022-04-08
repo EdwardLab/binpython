7cb8ac7a65eaa34a1e2f400a87457017f13466ee
xingyujie <39870096+xingyujie@users.noreply.github.com>
2022-04-08 22:53:29 +0800

    Update binpython.py
    Add many things

--- binpython.py
+++ binpython.py
@@ -1,33 +1,67 @@
-#BINPython By:XINGYUJIE AGPL-V3.0 LECENSE Release
-#Please follow the LICENSE
-ver="0.15-beta"
+"""
+ @header binpython.py
+ @author xingyujie
+ @abstract BINPython main file
+"""
+#BINPython By:XINGYUJIE AGPL-V3.0 LICENSE Release
+#Please follow the LICENSE AGPL-V3
+
+####################################
+#configure
+
+ver="0.15-stable"
+#Don't change
+
+libs_warning="1"  
+#1 is ture 0 is false.
+#Changing the value to 0 will close the prompt that the library does not exist
+
+releases_ver="offical"
+importlibs="os" #Don't use "import xxx"
+#importlibs="<library>"
+#Imported library name, please use "importlibs="<library name>" instead of "import <library name>"
+
+
+####################################
+def self_import(name):
+    __import__(name)
+try:
+    self_import(importlibs)
+except ImportError:
+    if libs_warning == "1":
+        print("Warning: Custom import library %s does not exist, please check the source code library configuration and rebuild" % importlibs)
+        print("")
+try:
 #base import
-import getopt
-import sys
-import platform
-#fix some libs
-from sys import exit
+    import getopt
+    import sys
+    import platform
+#fix for exit()
+    from sys import exit
 #import for http_server
-import http.server
-import socketserver
+    import http.server
+    import socketserver
 #except ImportError:
-   
+except ImportError:
+    print("Unable to use any library, the program does not work properly, please rebuild")
 #gui import
 try:
     import tkinter
     import turtle
 #warning for gui
 except ImportError:
-    print("Warning: Some GUI (graphical) libraries for BINPython do not exist, such as tkinter and turtle.  Because they are not built when they are built.  If you need to fix this warning, please complete the support libraries imported in the source code at build time (use pip or build it yourself), if your system does not support these libraries, you can remove or change this hint in the source code and rebuild")
-    print("")
+    if libs_warning == "1":
+        print("Warning: Some GUI (graphical) libraries for BINPython do not exist, such as tkinter and turtle.  Because they are not built when they are built.  If you need to fix this warning, please complete the support libraries imported in the source code at build time (use pip or build it yourself), if your system does not support these libraries, you can remove or change this hint in the source code and rebuild")
+        print("")
 
 #import math
 try:
     import fractions
     import cmath
 except ImportError:
-    print("Warning: Some math or computation libraries for BINPython do not exist, such as fractions and cmath.  Because they weren't built when they were built.  If you need to fix this warning, please complete the support libraries imported in the source code when building (use pip or build it yourself), if your system does not support these libraries, you can remove or change this prompt in the source code and rebuild")
-    print("")
+    if libs_warning == "1":
+        print("Warning: Some math or computation libraries for BINPython do not exist, such as fractions and cmath.  Because they weren't built when they were built.  If you need to fix this warning, please complete the support libraries imported in the source code when building (use pip or build it yourself), if your system does not support these libraries, you can remove or change this prompt in the source code and rebuild")
+        print("")
 #rlcompleter
 #import for normal
 try:
@@ -35,14 +69,16 @@ try:
     import rlcompleter
     import array
 except ImportError:
-    print("Warning: Some libraries for functions, data types, etc. for BINPython do not exist, such as rlcomplter and array.  Because they weren't built when they were built.  If you need to fix this warning, please complete the support libraries imported in the source code when building (use pip or build it yourself), if your system does not support these libraries, you can remove or change this prompt in the source code and rebuild")
-    print("")
+    if libs_warning == "1":
+        print("Warning: Some libraries for functions, data types, etc. for BINPython do not exist, such as rlcomplter and array.  Because they weren't built when they were built.  If you need to fix this warning, please complete the support libraries imported in the source code when building (use pip or build it yourself), if your system does not support these libraries, you can remove or change this prompt in the source code and rebuild")
+        print("")
 try:
      import filecmp
      import tempfile
 except ImportError:
-     print("Warning: Some file manipulation libraries for BINPython do not exist, such as filecmp and tempfile.  Because they weren't built when they were built.  If you need to fix this warning, please complete the support libraries imported in the source code when building (use pip or build it yourself), if your system does not support these libraries, you can remove or change this prompt in the source code and rebuild")
-     print("")
+    if libs_warning == "1":
+        print("Warning: Some file manipulation libraries for BINPython do not exist, such as filecmp and tempfile.  Because they weren't built when they were built.  If you need to fix this warning, please complete the support libraries imported in the source code when building (use pip or build it yourself), if your system does not support these libraries, you can remove or change this prompt in the source code and rebuild")
+        print("")
 #def
 def help():
     print("[*] BINPython Help")
@@ -65,7 +101,7 @@ for opt_name,opt_value in opts:
         help()
         sys.exit()
     if opt_name in ('-v','--version'):
-        print("BINPython " + ver + " By:XINGYUJIE[https://github.com/xingyujie/binpython] AGPL-3.0 LICENSE Release")
+        print("BINPython " + ver + "-" + releases_ver + " By:XINGYUJIE[https://github.com/xingyujie/binpython] AGPL-3.0 LICENSE Release")
         print("Python " + platform.python_version())
         exit()
     if opt_name in ('-f','--file'):
@@ -89,7 +125,7 @@ with socketserver.TCPServer(("", PORT), Handler) as httpd:
         sys.exit()
 #main BINPython
 
-print("BINPython " + ver + " (Python Version:" + platform.python_version() + ")By:XINGYUJIE https://github.com/xingyujie/binpython[Running on " + platform.platform() + " " + platform.version() + "]")
+print("BINPython " + ver + "-" + releases_ver + " (Python Version:" + platform.python_version() + ")By:XINGYUJIE https://github.com/xingyujie/binpython[Running on " + platform.platform() + " " + platform.version() + "]")
 print('Type "about", "help", "copyright", "credits" or "license" for more information.')
 try:
     while True:
